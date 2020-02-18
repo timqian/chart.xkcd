4b08d77703e0f72c534247b8d9ff76bdb56a3c3a
leoknight <lichenghua0@outlook.com>
2020-02-19 01:49:15 +0800

    line chart: fix error if title is null

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -141,7 +141,7 @@ class Tooltip {
     const maxItemLength = this.items.reduce(
       (pre, cur) => (pre > cur.text.length ? pre : cur.text.length), 0,
     );
-    const maxLength = Math.max(maxItemLength, this.title.length);
+    const maxLength = this.title ? Math.max(maxItemLength, this.title.length) : maxItemLength;
 
     return maxLength * 7.4 + 25;
   }
